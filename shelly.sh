49679d74e0d6352e26ad8238131e85d0ade33769
sam <your.email@here.com>
2018-07-07 19:52:57 -0700

    fixed script

--- shelly.sh
+++ shelly.sh
@@ -12,6 +12,11 @@ git clone https://github.com/m0sf3tz/pru-cbuf-fw.git
 cp pru-cbuf-fw/Debug/for_real.out .
 cp pru-cbuf-fw/Debug/for_real.map .
 
+python splitter.py for_real.out for_real.map
 
-
+rm -f bin1
+rm -f bin2
+rm -f bin3
+rm  -f bin4
+rm -f bin5
 
